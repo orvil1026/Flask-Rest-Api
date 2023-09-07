f69c602b2c4dbbdde43eefd0ef6c458816fbf8f5
Orvil D'silva <orvildsilva@gmail.com>
2023-09-07 09:37:51 +0530

    Users GET Request Added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, flash, abort, request, jsonify
 from pymongo import MongoClient
 from flask_restful import Resource, Api, reqparse
+from bson import json_util
+import json
+import pprint
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,8 +13,18 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['test-database']
 collection = db['test-collection']
 
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
 
 class Users(Resource):
+
+    def get(self):
+        result = []
+        for user in collection.find():
+            result.append(user)
+            
+        return parse_json(result)    
+
     def post(self):
         
         parser = reqparse.RequestParser()
@@ -20,26 +33,24 @@ class Users(Resource):
         parser.add_argument('email', required=True, help="Name cannot be blank!")
 
         args = parser.parse_args()
-        print("USERNAME::::::s"+args['username'])
+        
         collection.insert_one({'username': args['username'], 'email': args['email'], 'password': args['password']})
         return jsonify(args)
         
-    
 
+# @app.route('/add_data')
+# def add_data():
+#     collection.insert_one({'name': 'John'})
+#     return 'Added Data!'
 
-@app.route('/add_data')
-def add_data():
-    collection.insert_one({'name': 'John'})
-    return 'Added Data!'
+# @app.route('/add_data1')
+# def add_data1():
+#     collection.insert_one({'name': 'Orvil'})
+#     return 'Added Data!'
 
-@app.route('/add_data1')
-def add_data1():
-    collection.insert_one({'name': 'Orvil'})
-    return 'Added Data!'
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
 
 
 api.add_resource(Users, '/users')
